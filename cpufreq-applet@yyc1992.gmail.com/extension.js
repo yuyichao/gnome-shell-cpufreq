6a60e28d00621f398536f9e46458ed7bd589a621
Yichao Yu <yyc1992@gmail.com>
2011-06-27 22:55:04 -0400

    read from sys

--- cpufreq-applet@yyc1992.gmail.com/extension.js
+++ cpufreq-applet@yyc1992.gmail.com/extension.js
@@ -19,9 +19,93 @@
 // Author: Yichao Yu
 // Email: yyc1992@gmail.com
 
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const St = imports.gi.St;
 const Shell = imports.gi.Shell;
 const Lang = imports.lang;
 
+const cpu_path = '/sys/devices/system/cpu/';
+const cpu_dir = Gio.file_new_for_path(cpu_path);
+let cpus = [];
+let selectors = [];
+
+function listdir(dir) {
+    let enumerator = dir.enumerate_children(Gio.FILE_ATTRIBUTE_STANDARD_NAME, Gio.FileQueryInfoFlags.NONE, null);
+    let children = [];
+    let child;
+    while ((child = enumerator.next_file(null)))
+        children.push(child.get_name());
+    return children;
+}
+
+function get_cpus() {
+    let cpu_child = listdir(cpu_dir);
+    let cpus = [];
+    let pattern = /^cpu[0-9]+/
+    for (let i in cpu_child)
+        if (pattern.test(cpu_child[i]))
+            cpus.push(cpu_child[i]);
+    return cpus;
+}
+
+function parseInts(strs) {
+    let rec = [];
+    for (let i in strs)
+        rec.push(parseInt(strs[i]));
+    return rec;
+}
+
+function rd_frm_file(file) {
+    return Shell.get_file_contents_utf8_sync(file).replace(/\n/g, '').replace(/ +/g, ' ').replace(/ +$/g, '').split(' ');
+}
+
+function rd_nums_frm_file(file) {
+    return parseInts(rd_frm_file(file));
+}
+
+function Cpufreq_Selector() {
+    this._init.apply(this, arguments);
+}
+
+Cpufreq_Selector.prototype = {
+    _init: function(cpu) {
+        this.cpunum = cpu.replace(/cpu/, '');
+        this.cpufreq_path = cpu_path + '/' + cpu + '/cpufreq/';
+        this.get_freqs();
+        this.get_governors();
+        this.get_cur_freq();
+        this.get_cur_governor();
+    },
+    get_freqs: function() {
+        this.max = rd_nums_frm_file(this.cpufreq_path + '/scaling_max_freq')[0];
+        this.min = rd_nums_frm_file(this.cpufreq_path + '/scaling_min_freq')[0];
+        this.avail_freqs = rd_nums_frm_file(this.cpufreq_path + '/scaling_available_frequencies');
+    },
+    get_governors: function() {
+        this.avail_governors = rd_frm_file(this.cpufreq_path + '/scaling_available_governors');
+    },
+    get_cur_freq: function() {
+        this.cur_freq = rd_nums_frm_file(this.cpufreq_path + '/scaling_cur_freq')[0];
+    },
+    get_cur_governor: function() {
+        this.cur_governor = rd_frm_file(this.cpufreq_path + '/scaling_governor')[0];
+    },
+    set_freq: function(index) {
+        GLib.spawn_sync('.', ['cpufreq-selector', '-c', this.cpunum, '-f', this.avail_freqs[index].toString()], [], GLib.SpawnFlags.SEARCH_PATH, null);
+    },
+    set_governor: function(index) {
+        GLib.spawn_sync('.', ['cpufreq-selector', '-c', this.cpunum, '-g', this.avail_governors[index]], [], GLib.SpawnFlags.SEARCH_PATH, null);
+    }
+};
 
 function main() {
+    cpus = get_cpus();
+    for (let i in cpus) {
+        selectors[i] = new Cpufreq_Selector(cpus[i]);
+        for (let j in selectors[i]) {
+            if (typeof(selectors[i][j]) != 'function')
+                print(selectors[i][j]);
+        }
+    }
 }
